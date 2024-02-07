bc934974cf83d1d20cd079341823e724358f300e
zepeersGily <>
2024-02-07 12:20:12 +0100

    added a custom hook fetching data

---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
+
+const rapid_api_key = RAPID_API_KEY;
+
+export default function useFetch(endpoint, query) {
+   const [data, setData] = useState([]);
+   const [isLoading, setIsLoading] = useState(false);
+   const [isError, setIsError] = useState(false);
+
+   const options = {
+      method: 'GET',
+      url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+      headers: {
+         'X-RapidAPI-Key': rapid_api_key,
+         'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      },
+      params: {...query}
+   };
+
+   async function fetchData() {
+
+      setIsLoading(true);
+
+      try {
+         const response = await axios.request(options)
+         setData(response.data.data);
+         setIsLoading(false);
+      } catch (error) {
+         setIsError(true);
+         alert('There is an error');
+      } finally {
+         setIsLoading(false);
+      }
+
+   }
+
+   useEffect(() => {
+      fetchData()
+   }, []);
+
+   function refetch() {
+      fetchData();
+   }
+
+   return {data, isLoading, isError, refetch};
+}
