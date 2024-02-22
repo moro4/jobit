a272520e1f9f845730c60105f6208ef0f84cef0a
zepeersGily <>
2024-02-22 18:23:10 +0100

    implemented the header section in the job details screen

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import styles from './company.style'
+import { icons } from '../../../constants/'
+import { isImageAvailable } from '../../../utils'
 
-const Company = () => {
-  return (
-    <View>
-      <Text>Company</Text>
-    </View>
-  )
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
+
+   const fallbackLogoUrl = 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg';
+
+   return (
+      <View style={styles.container}>
+
+         <View style={styles.logoBox}>
+            <Image
+               style={styles.logoImage}
+               resizeMode='contain'
+               source={{
+                  uri: isImageAvailable(companyLogo)
+                     ? companyLogo
+                     : fallbackLogoUrl
+               }}
+            />
+         </View>
+
+         <View style={styles.jobTitleBox}>
+            <Text style={styles.jobTitle}>{jobTitle}</Text>
+         </View>
+
+         <View style={styles.companyInfoBox}>
+
+            <Text style={styles.companyName}>
+               {companyName} /
+            </Text>
+
+            <View style={styles.locationBox}>
+               <Image
+                  source={icons.location}
+                  style={styles.locationImage}
+                  resizeMode='contain'
+               />
+               <Text style={styles.locationName}>{location}</Text>
+            </View>
+
+         </View>
+
+      </View>
+   )
 }
 
 export default Company
